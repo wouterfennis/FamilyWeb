e57d86ba5a127fee209bf847e5e746fecb6c4179
Wouter Fennis <Wouter Fennis>
2016-02-28 13:43:50 +0100

    New commit

--- src/main/java/servlets/FamilyWeb/ContactServlet.java
+++ src/main/java/servlets/FamilyWeb/ContactServlet.java
@@ -18,137 +18,140 @@ import domain.FamilyWeb.User;
  * Servlet implementation class ContactServlet
  */
 public class ContactServlet extends HttpServlet {
+	public static final String[] CONTACTGROUPS = {"household", "family", "friends",
+			"colleagues", "neighbours", "acquaintance", "education",
+			"club", "religion", "careinstitution", "youthcare",
+			"bureauhalt", "justice"};
 	private static final long serialVersionUID = 1L;
+	public static final String SOCIALWORKER_FAMILY_NEW_NETWORK_CONTACTS_JSP = "/socialworker/family/new_network_contacts.jsp";
+	public static final String SOCIALWORKER_FAMILY_NEW_NETWORK_QUESTIONS_JSP = "/socialworker/family/new_network_questions.jsp";
+	public static final String CANT_LOAD_QUESTIONS = "Kon de vragenlijst niet inladen.";
+	public static final String ENTER_AT_LEAST_ONE_CONTACT = "Voer minimaal één contact in voor de vragenlijst.";
+	public static final String WRONG_DATA_WITH_CONTACTS = "Gegevens kloppen niet van één of meerdere contacten.";
 	private User user = null;
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-	}
+	
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// get the user from the session
 		user = (User) req.getSession().getAttribute("user");
 		RequestDispatcher reqDisp = null;
-		boolean b = false;
+		boolean contactsAreValid = false;
 		ArrayList<Contact> contacts = null;		
 		if (user != null) {
-			b = true;
+			contactsAreValid = true;
 			// define hardcoded groups
-			String[] contactgroups = { "household", "family", "friends",
-					"colleagues", "neighbours", "acquaintance", "education",
-					"club", "religion", "careinstitution", "youthcare",
-					"bureauhalt", "justice" };
 			contacts = new ArrayList<Contact>();
 			int j =0;
-			int id = 1;
-			// search for each group
-			for (String group : contactgroups) {
-				j++;
-				// get amount of total created contacts in the group
-				int contactsInGroup = Integer.valueOf(req.getParameter(
-						"counter" + group));
-				// for each contact created of group, get each input
-				for (int i = 1; i <= contactsInGroup; i++) {
-					// check if the contact has to be created
-					String validate = req.getParameter(group + "validate" + i)
-							.trim();
-					// contact has to be created
-					if (validate.equals("true")) {
-						// get all input
-						String name = req.getParameter(group + "name" + i)
-								.trim();
-						String commentary = "";
-						String com = req.getParameter(group + "comment" + i).toString();
-						if(com != null && !com.trim().equals(""))
-							commentary = com.trim();
-						String role = req.getParameter(group + "role" + i)
-								.trim();
-						// convert age from input 
-						String ageS = req.getParameter(group + "age" + i)
-								.trim();
-						int age = 0;
-						try {
-							age = Integer.valueOf(ageS);
-						} catch (NumberFormatException e) {
-						}
-						// validate contact, if it went well create contact
-						if (checkContact(name, role, age)){
-							Contact c = new Contact(name, commentary, role,
-									age, group,j);
-							c.setContact_id(id);
-							id++;
-							contacts.add(c);							
-						}else
-							b = false;
-					}
-				}
-			}
+			int idOfContact = 1;
+			// validate each group
+			contactsAreValid = validateContacts(req, contactsAreValid, contacts, j, idOfContact);
 		}
 		// check if there are contacts created
 		if(contacts.isEmpty()){
-			b = false;
+			contactsAreValid = false;
 		}
 		// if contacts are created, set survey attributes
-		if (b) {
+		if (contactsAreValid) {
 			// get interviewee
 			String interviewee = req.getParameter("interviewee");
 			String[] parts = interviewee.split(":");
 			String nameInterviewee = parts[0];
-			int IDInterviewee = Integer.parseInt(parts[1]);
+			int IDOfInterviewee = Integer.parseInt(parts[1]);
 			// get client from session
 			Client client = (Client) req.getSession().getAttribute("client");
 			// if interviewee is the one that was selected, set that into the session
-			if (client.getClient_id() == IDInterviewee
+			if (client.getClient_id() == IDOfInterviewee
 					&& client.getForename().equals(nameInterviewee)) {
-				req.getSession().setAttribute("intervieweeC", IDInterviewee);
+				req.getSession().setAttribute("intervieweeC", IDOfInterviewee);
 				req.getSession().setAttribute("intervieweeF", 0);
 			} 
 			// if interviewee is an familymember, set that into the session
 			else {
-				req.getSession().setAttribute("intervieweeF", IDInterviewee);
+				req.getSession().setAttribute("intervieweeF", IDOfInterviewee);
 				req.getSession().setAttribute("intervieweeC", 0);
 			}
 			// setup the survey from the select box
 			String surveyName = req.getParameter("survey");
 			Survey survey = user.getDbController().getSurvey(surveyName);
-			if (!b && survey == null) {
+			if (!contactsAreValid && survey == null) {
 				// set error message, because survey wasn't loaded well and return user to contacts page
-				req.setAttribute("message", "Kon de vragenlijst niet inladen.");
+				req.setAttribute("message", CANT_LOAD_QUESTIONS);
 				req.setAttribute("messageType", "error");
 				reqDisp = req
-						.getRequestDispatcher("/socialworker/family/new_network_contacts.jsp");
+						.getRequestDispatcher(SOCIALWORKER_FAMILY_NEW_NETWORK_CONTACTS_JSP);
 			} 
 			// send user to next page for survey
-			else if (b) {
+			else if (contactsAreValid) {
 				req.getSession().setAttribute("survey", survey);				
 				reqDisp = req
-						.getRequestDispatcher("/socialworker/family/new_network_questions.jsp");
+						.getRequestDispatcher(SOCIALWORKER_FAMILY_NEW_NETWORK_QUESTIONS_JSP);
 			}
 			req.getSession().setAttribute("contacts", contacts);
 		} 
-		// wasn't created any contacts
+		// no contact was created. Throw warning
 		else if(contacts.isEmpty()){
 			req.setAttribute("message",
-					"Voer minimaal één contact in voor de vragenlijst.");
+					ENTER_AT_LEAST_ONE_CONTACT);
 			req.setAttribute("messageType", "warning");
 			reqDisp = req
-					.getRequestDispatcher("/socialworker/family/new_network_contacts.jsp");
+					.getRequestDispatcher(SOCIALWORKER_FAMILY_NEW_NETWORK_CONTACTS_JSP);
 		}
-		// some input missing
+		// not all fields have been filled. Throw warning
 		else {
 			req.setAttribute("message",
-					"Gegevens kloppen niet van één of meerdere contacten.");
+					WRONG_DATA_WITH_CONTACTS);
 			req.setAttribute("messageType", "warning");
 			reqDisp = req
-					.getRequestDispatcher("/socialworker/family/new_network_contacts.jsp");
+					.getRequestDispatcher(SOCIALWORKER_FAMILY_NEW_NETWORK_CONTACTS_JSP);
 
 		}
 		reqDisp.forward(req, resp);
+	}
+
+	private boolean validateContacts(HttpServletRequest req, boolean b, ArrayList<Contact> contacts, int j, int id) {
+		for (String group : CONTACTGROUPS) {
+            j++;
+            // get amount of total created contacts in the group
+            int contactsInGroup = Integer.parseInt((req.getParameter(
+                    "counter" + group)));
+            // for each contact created of group, get each input
+            for (int i = 1; i <= contactsInGroup; i++) {
+                // check if the contact has to be created
+                String validate = req.getParameter(group + "validate" + i)
+                        .trim();
+                // contact has to be created
+                if (validate.equals("true")) {
+                    // get all input
+                    String name = req.getParameter(group + "name" + i)
+                            .trim();
+                    String commentary = "";
+                    String com = req.getParameter(group + "comment" + i);
+                    if(com != null && !com.trim().equals(""))
+                        commentary = com.trim();
+                    String role = req.getParameter(group + "role" + i)
+                            .trim();
+                    // convert age from input
+                    String ageS = req.getParameter(group + "age" + i)
+                            .trim();
+                    int age = 0;
+                    try {
+                        age = Integer.valueOf(ageS);
+                    } catch (NumberFormatException e) {
+                    }
+                    // validate contact, if it went well create contact
+                    if (checkContact(name, role, age)){
+                        Contact c = new Contact(name, commentary, role,
+                                age, group,j);
+                        c.setContact_id(id);
+                        id++;
+                        contacts.add(c);
+                    }else
+                        b = false;
+                }
+            }
+        }
+		return b;
 	}
 
 	/**
--- src/main/java/servlets/FamilyWeb/PasswordresetServlet.java
+++ src/main/java/servlets/FamilyWeb/PasswordresetServlet.java
@@ -24,70 +24,87 @@ public class PasswordresetServlet extends HttpServlet {
 	private final String PAGE_STARTSCREEN_ADMINISTRATOR = "/administrator/startscreen_administrator.jsp";
 	private final String PAGE_STARTSCREEN_SOCIALWORKER = "/socialworker/startscreen_socialworker.jsp";
 	
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
 	
-		this.req = req;
+		this.req = request;
 		// get the currentUser 
-		Object cUser = req.getSession().getAttribute("user");
-		User currentUser = (cUser instanceof Administrator) ? (Administrator) cUser : (Socialworker) cUser;		
+		User currentUser = getUser(request);
 		
 		// check if password is not null
-		if (req.getParameter("old_password") != null && req.getParameter("new_password") != null && req.getParameter("new_password_confirmation") != null) {
+		if (request.getParameter("old_password") != null && request.getParameter("new_password") != null && request.getParameter("new_password_confirmation") != null) {
 			
-			String oldpassword = req.getParameter("old_password").trim();
-			String newpassword = req.getParameter("new_password").trim();
-			String newpasswordConfirm = req.getParameter("new_password_confirmation").trim();
+			String oldPassword = request.getParameter("old_password").trim();
+			String newPassword = request.getParameter("new_password").trim();
+			String newPasswordConfirm = request.getParameter("new_password_confirmation").trim();
 			
 			// check if old password is valid and new password is equal to new password confirmation
-			if (oldpassword.equals(currentUser.getPassword()) && newpassword.equals(newpasswordConfirm)) {
-				
-				// Update user in database and set password reset to false
-				currentUser.setPassword(newpassword);
-				currentUser.setWwreset(false);
-				currentUser.updateDB();
-				
-				// Refresh the user in session
-				req.getSession().setAttribute("user", currentUser.getDbController().getUser(currentUser.getUsername()));
-				this.setMessage("success", "Succesvol wachtwoord veranderd.");
-				
-				// Check if user is administrator
-				if (currentUser instanceof Administrator) {
-					try {
-					// load/set users and clients in overview tables
-					req.getSession().setAttribute("usersJSON", OverviewController.getInstance().RefreshOverviewUsers(currentUser));
-					req.getSession().setAttribute("clientsJSON", OverviewController.getInstance().RefreshOverviewClients(currentUser));
-					// load/set users for autocomple client add/edit page
-					req.getSession().setAttribute("users", OverviewController.getInstance().autoComplete(currentUser));
-					req.getRequestDispatcher(PAGE_STARTSCREEN_ADMINISTRATOR).forward(req, resp);
-					} catch (JSONException e) {
-						this.setMessage("error", "Kon de gegevens niet goed inladen, probeer opnieuw in te loggen.");
-						req.getRequestDispatcher(PAGE_LOGIN).forward(req, resp);
-					}
-				// User is socialworker
-				} else {
-					try {
-					// load/set clients in overview tables
-					req.getSession().setAttribute("clientsJSON", OverviewController.getInstance().RefreshOverviewClients(currentUser));
-					req.getSession().setAttribute("clients", currentUser.getDbController().getAllClientsOfUser(currentUser));
-					req.getRequestDispatcher(PAGE_STARTSCREEN_SOCIALWORKER).forward(req, resp);
-					} catch (JSONException e) {
-						this.setMessage("error", "Kon de gegevens niet goed inladen, probeer opnieuw in te loggen.");
-						req.getRequestDispatcher(PAGE_LOGIN).forward(req, resp);
-					}		
-				}
-				
-			// input not valid, password combination is wrong
-			} else {
-				this.setMessage("warning", "Wachtwoord onjuist of wachtwoorden komen niet overeeen.");
-				req.getRequestDispatcher(PAGE_PASSWORD_RESET).forward(req, resp);
-			}
-		
-		// input not valid, password is null or empty
+			validatePassword(request, resp, currentUser, oldPassword, newPassword, newPasswordConfirm);
+
+			// input not valid, password is null or empty
 		} else {
 			this.setMessage("warning", "Alle velden dienen ingevuld te worden.");
-			req.getRequestDispatcher(PAGE_PASSWORD_RESET).forward(req, resp);
+			request.getRequestDispatcher(PAGE_PASSWORD_RESET).forward(request, resp);
 		}
 	}
+
+	private void validatePassword(HttpServletRequest request, HttpServletResponse resp, User currentUser, String oldPassword, String newPassword, String newPasswordConfirm) throws ServletException, IOException {
+		if (oldPassword.equals(currentUser.getPassword()) && newPassword.equals(newPasswordConfirm)) {
+
+            // Update user in database and set password reset to false
+            currentUser.setPassword(newPassword);
+            currentUser.setWwreset(false);
+            currentUser.updateDB();
+
+            // Refresh the user in session
+            request.getSession().setAttribute("user", currentUser.getDbController().getUser(currentUser.getUsername()));
+            this.setMessage("success", "Succesvol wachtwoord veranderd.");
+
+            // Check if user is administrator
+            if (currentUser instanceof Administrator) {
+                setAdministratorClients(request, resp, currentUser);
+                // User is socialworker
+            } else {
+                setSocialworkerClients(request, resp, currentUser);
+            }
+
+        // input not valid, password combination is wrong
+        } else {
+            this.setMessage("warning", "Wachtwoord onjuist of wachtwoorden komen niet overeeen.");
+            request.getRequestDispatcher(PAGE_PASSWORD_RESET).forward(request, resp);
+        }
+	}
+
+	private void setAdministratorClients(HttpServletRequest request, HttpServletResponse resp, User currentUser) throws ServletException, IOException {
+		try {
+        // load/set users and clients in overview tables
+        request.getSession().setAttribute("usersJSON", OverviewController.getInstance().RefreshOverviewUsers(currentUser));
+        request.getSession().setAttribute("clientsJSON", OverviewController.getInstance().RefreshOverviewClients(currentUser));
+        // load/set users for autocomple client add/edit page
+        request.getSession().setAttribute("users", OverviewController.getInstance().autoComplete(currentUser));
+        request.getRequestDispatcher(PAGE_STARTSCREEN_ADMINISTRATOR).forward(request, resp);
+        } catch (JSONException e) {
+            this.setMessage("error", "Kon de gegevens niet goed inladen, probeer opnieuw in te loggen.");
+            request.getRequestDispatcher(PAGE_LOGIN).forward(request, resp);
+        }
+	}
+
+	private void setSocialworkerClients(HttpServletRequest request, HttpServletResponse resp, User currentUser) throws ServletException, IOException {
+		try {
+        // load/set clients in overview tables
+        request.getSession().setAttribute("clientsJSON", OverviewController.getInstance().RefreshOverviewClients(currentUser));
+        request.getSession().setAttribute("clients", currentUser.getDbController().getAllClientsOfUser(currentUser));
+        request.getRequestDispatcher(PAGE_STARTSCREEN_SOCIALWORKER).forward(request, resp);
+        } catch (JSONException e) {
+            this.setMessage("error", "Kon de gegevens niet goed inladen, probeer opnieuw in te loggen.");
+            request.getRequestDispatcher(PAGE_LOGIN).forward(request, resp);
+        }
+	}
+
+	private User getUser(HttpServletRequest request) {
+		Object currentUserObject = request.getSession().getAttribute("user");
+		return (currentUserObject instanceof Administrator) ? (Administrator) currentUserObject : (Socialworker) currentUserObject;
+	}
+
 	/**
 	 * Method to set information message on page.
 	 * @param messageType String type of message could be success, error or warning
